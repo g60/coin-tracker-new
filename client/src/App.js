315ac2f14fcd611c71ebda8eadae91c6ae059442
g60 <davidletts@gmail.com>
2018-05-14 10:17:25 +0100

    Added express server

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,85 @@ import CoinList from './components/CoinList';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      passwords: [],
+      //coinData: [],
+      //coinTransactions: [],           
+      allTransactions: [],
+    };
+
+  }
+
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getPasswords();
+    this.getTransactions();
+  }
+
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
+  }
+
+  getTransactions = () => {
+    // Get the passwords and store them in state
+    fetch('/api/transactions')
+      .then(res => res.json())
+      .then(allTransactions => this.setState({ allTransactions }));
+  }
+
   render() {
+
+    const { passwords } = this.state;
+
     return (
       <div className="App">
         
-        <CoinList />
+        <CoinList allTransactions={this.state.allTransactions} />
+
+        <div className="App">
+                {/* Render the passwords if we have them */}
+                {passwords.length ? (
+                  <div>
+                    <h1>5 Passwords.</h1>
+                    <ul className="passwords">
+                      {/*
+                        Generally it's bad to use "index" as a key.
+                        It's ok for this example because there will always
+                        be the same number of passwords, and they never
+                        change positions in the array.
+                      */}
+                      {passwords.map((password, index) =>
+                        <li key={index}>
+                          {password}
+                        </li>
+                      )}
+                    </ul>
+                    <button
+                      className="more"
+                      onClick={this.getPasswords}>
+                      Get More
+                    </button>
+                  </div>
+                ) : (
+                  // Render a helpful message otherwise
+                  <div>
+                    <h1>No passwords :(</h1>
+                    <button
+                      className="more"
+                      onClick={this.getPasswords}>
+                      Try Again?
+                    </button>
+                  </div>
+                )}
+              </div>
 
       </div>
     );
--- index.js
+++ index.js
@@ -16,14 +16,126 @@ app.get('/api/passwords', (req, res) => {
   const passwords = Array.from(Array(count).keys()).map(i =>
     generatePassword(12, false)
   )
-    */
+  */
 
- const passwords = ["123", "456", "789"];
+  const passwords = ["123", "456", "789"];
 
   // Return them as json
   res.json(passwords);
 
   console.log(`Sent ${count} passwords`);
+});
+
+// Put all API endpoints under '/api'
+app.get('/api/transactions', (req, res) => {
+  
+  const allTransactions = [
+    {
+        symbol: "XRP",
+        dateBought: "2017-11-20", 
+        amountBought: 255.7776,
+        priceBoughtAt: 0.19531860370636582393199787493359,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "David",
+    },
+    {
+        symbol: "LTC",
+        dateBought: "2017-11-18", 
+        amountBought: 0.92701669,
+        priceBoughtAt: 51.57,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "David",
+    },
+    {
+        symbol: "ETH",
+        dateBought: "2017-11-18", 
+        amountBought: 0.18720073,
+        priceBoughtAt: 255.39,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "David",
+    },
+    {
+        symbol: "BTC",
+        dateBought: "2017-11-18", 
+        amountBought: 0.00803233,
+        priceBoughtAt: 5952.20,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "David",
+    },
+    {
+        symbol: "ETH",
+        dateBought: "2017-11-18", 
+        amountBought: 0.18515643,
+        priceBoughtAt: 258.21,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "David",
+    },
+    {
+        symbol: "BTC",
+        dateBought: "2017-12-06", 
+        amountBought: 0.03379108,
+        priceBoughtAt: 10018.62,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "David",
+    },
+    {//MICHAEL
+        symbol: "BTC",
+        dateBought: "2017-12-11", 
+        amountBought: 0.00369789,
+        priceBoughtAt: 12928.99,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "Michael",
+    },
+    {//MICHAEL
+        symbol: "LTC",
+        dateBought: "2017-12-11", 
+        amountBought: 0.38003860,
+        priceBoughtAt: 125.80,
+        dateSold: "",
+        amountSold: "",
+        priceSoldAt: "",
+        name: "Michael",
+    },
+    {
+        symbol: "BTC",
+        dateBought: "", 
+        amountBought: 0,
+        priceBoughtAt: 0,
+        dateSold: "2017-12-11",
+        amountSold: 0,
+        priceSoldAt: 0,
+        name: "David1",
+    },
+    {
+        symbol: "XRP",
+        dateBought: "", 
+        amountBought: 0,
+        priceBoughtAt: 0,
+        dateSold: "2017-12-11",
+        amountSold: 50,
+        priceSoldAt: 100,
+        name: "",
+    },
+  ];
+
+  res.json(allTransactions);
+
+  console.log(`Sent ${allTransactions.length} transactions`);
+
 });
 
 // The "catchall" handler: for any request that doesn't
